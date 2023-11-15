5d2f927f48f79d7bf62423f0cc81ae9224572d56
LarisaM16 <124587607+LarisaM16@users.noreply.github.com>
2023-11-15 22:07:27 +0200

    add azur and jumbo

---
+++ sites/jumbo_scraper.py
@@ -0,0 +1,134 @@
+#
+#
+#  Basic for scraping data from static pages
+#  ... project made by Andrei Cojocaru
+#  LinkedIn: https://www.linkedin.com/in/andrei-cojocaru-985932204/
+#  Github: https://github.com/andreireporter13
+#
+# ------ IMPORTANT! ------
+# if you need return soup object:
+# you cand import from __utils -> GetHtmlSoup
+# if you need return regex object:
+# you cand import from __utils ->
+# ---> get_data_with_regex(expression: str, object: str)
+#
+# Company ---> jumbo
+# Link ------> https://corporate.e-jumbo.gr/ro/job-opportunities/theseis-ergasias/
+#
+#
+from __utils import (
+    GetStaticSoup,
+    get_county,
+    get_job_type,
+    Item,
+    UpdateAPI,
+)
+
+
+'''
+    Daca te-ai deprins cu aceasta formula de cod,
+    atunci poti sterge acest comentariu din fisierul
+    __create_scraper.py, din functia -> create_static_scraper_config <-
+
+    Deci:
+    ########################################################################
+    1) --->  clasa GetStaticSoup returneaza un obiect BeautifulSoup,
+    direct in instanta, fara a apela alte metode.
+
+    soup = GetStaticSoup(link) -> si gata, ai acces la obiectul soup
+    si deja poti face -> for job in soup.find_all(...)
+
+    + poti sa-i adaugi si custom_headers
+    soup = GetStaticSoup(link, custom_headers)
+    ... by default, custom_headers = None, dar in __utils ai un fisier
+    default_headers.py unde poti sa-ti setezi headerele tale default.
+
+    --------------IMPORTANT----------------
+    La nivel de proiect, ca o variabila globala, este definit Session()!
+    ... acest session inseamna ca orice clasa va putea folosi
+    ... aceeasi sesiune, practic se va evita multiple requests;
+
+    ########################################################################
+
+    2) ---> get_county(nume_localitat) -> returneaza numele judetului;
+    poti pune chiar si judetul, de exemplu, nu va fi o eroare.
+
+    ########################################################################
+
+    3) --->get_job_type(job_type: str) -> returneaza job_type-ul: remote,
+    hybrid, on-site
+
+    ########################################################################
+
+    4) ---> Item -> este un struct pentru datele pe care le vom stoca in lista
+    si, apoi, le vom trimite catre API.
+    exemplu: job_list.append(Item(job_title="titlu_str",
+                                    job_link="link",
+                                    company="nume_companie",
+                                    country="Romania",
+                                    county="Judetul",
+                                    city="Orasul",
+                                    remote="remote, onsite sau hibryd"))
+
+    ########################################################################
+
+    5) ---> clasa UpdateAPI are doua metode:
+    update_jobs(lista_dict_joburi) si update_logo(nume_companie, link_logo)
+
+    UpdateAPI().update_jobs(company_name: str, data_jobs: list)
+    UpdateAPI().update_logo(id_company: str, logo_link: str)
+
+    ########################################################################
+'''
+
+
+def scraper():
+    '''
+    ... scrape data from jumbo scraper.
+    '''
+    soup = GetStaticSoup("https://corporate.e-jumbo.gr/ro/job-opportunities/theseis-ergasias/")
+
+    # default location for this company
+    jumbo_locations = ["Bucuresti",
+                       "Timisoara", "Oradea",
+                       "Arad", "Ploiesti", "Pitesti",
+                       "Constanta", "Suceava", "Bacau", "Braila",
+                       "Brasov", "Craiova",
+                       ]
+
+    job_list = []
+    for job in soup.find_all('article', attrs={'class': 'x-control x-box x-article-box careers-article'}):
+
+        # get jobs items from response
+        job_list.append(Item(
+            job_title=job.find('h2', attrs={'class': 'title'}).text.strip(),
+            job_link='https://corporate.e-jumbo.gr' + job.find('a', attrs={'class': 'view-more'})['href'],
+            company='Jumbo',
+            country='Romania',
+            county=[get_county(town) for town in jumbo_locations],
+            city=jumbo_locations,
+            remote='on-site',
+        ).to_dict())
+
+    return job_list
+
+
+def main():
+    '''
+    ... Main:
+    ---> call scraper()
+    ---> update_jobs() and update_logo()
+    '''
+
+    company_name = "jumbo"
+    logo_link = "https://corporate.e-jumbo.gr/uploads/images/logo.png"
+
+    jobs = scraper()
+    
+    # uncomment if your scraper done
+    UpdateAPI().update_jobs(company_name, jobs)
+    UpdateAPI().update_logo(company_name, logo_link)
+
+
+if __name__ == '__main__':
+    main()
