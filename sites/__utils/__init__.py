9666b5b8fd8372c92d83dd3e0f0a67550b7018ea
LarisaM16 <124587607+LarisaM16@users.noreply.github.com>
2024-02-02 09:11:19 +0200

    update _init

--- sites/__utils/__init__.py
+++ sites/__utils/__init__.py
@@ -2,12 +2,12 @@
 #
 #
 #
-from scrapers.__utils.peviitor_update import UpdateAPI
-from scrapers.__utils.default_headers import DEFAULT_HEADERS
-from scrapers.__utils.items_struct import Item
-from scrapers.__utils.found_county import get_county
-from scrapers.__utils.req_bs4_shorts import GetStaticSoup, GetRequestJson, PostRequestsJson, HackCloudFlare,GetXMLObject
-from scrapers.__utils.req_bs4_shorts import GetHtmlSoup
-from scrapers.__utils.dynamic_requests_html_shorts import GetDynamicSoup
-from scrapers.__utils.get_job_type import get_job_type
-from scrapers.__utils.get_data_with_regex import get_data_with_regex
+from  __utils.peviitor_update import UpdateAPI
+from  __utils.default_headers import DEFAULT_HEADERS
+from  __utils.items_struct import Item
+from  __utils.found_county import get_county
+from  __utils.req_bs4_shorts import GetStaticSoup, GetRequestJson, PostRequestsJson, HackCloudFlare,GetXMLObject
+from  __utils.req_bs4_shorts import GetHtmlSoup
+from  __utils.dynamic_requests_html_shorts import GetDynamicSoup
+from  __utils.get_job_type import get_job_type
+from  __utils.get_data_with_regex import get_data_with_regex
